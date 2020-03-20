badb8882eb49992710015e3e52122d27dd5d89d6
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-03-20 12:40:44 -0400

    Added projects seed data

--- data/seeds/01-projects.js
+++ data/seeds/01-projects.js
@@ -1,13 +1,12 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('projects').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('projects').insert([
+        {project_name: 'Build ikea desk', project_completed: false, project_description: "Have to create an ikea desk"},
+        {project_name: 'Bake a cake'}
       ]);
     });
 };
